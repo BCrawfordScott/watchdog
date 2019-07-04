6b074b16afe7c1e4de8101c2548b8169d5bf7976
BCrawfordScott <BrianCrawfordScott@gmail.com>
2019-07-03 21:57:40 -0400

    Add top level tests

---
+++ __tests__/topLevel-test.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+describe('Application', () => {
+  it('Includes tests', () => {
+    expect(true).toEqual(true);
+  }); 
+
+  it('Includes a package.json', () => {
+    expect(fs.existsSync('./package.json')).toEqual(true);
+  });
+
+  it('Includes a .gitignore', () => {
+    expect(fs.existsSync('./.gitignore')).toEqual(true);
+  });
+
+  it('Includes a README.md', () => {
+    expect(fs.existsSync('./README.md')).toEqual(true);
+  });
+});
