f760f39bca3bdbc3c2265ead59409166c1e045dc
BCrawfordScott <bscott@appacademy.io>
2019-07-04 13:48:13 -0400

    Update kets and add mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const appKeys = require('./keys/keys');
 
 const app = express();
+const dbURI = appKeys.mongoDBURI;
 
-app.get("/", (req, res) => res.send("Hello World"));
+mongoose
+  .connect(dbURI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Watchdog says, \"Woof!\""));
 
 const port = process.env.PORT || 5000
 
