3ac5517d081cade2f66d7186f57dc7e7a4fdb45f
BCrawfordScott <bscott@appacademy.io>
2019-07-11 09:35:53 -0400

    add web token

--- services/authenticity.js
+++ services/authenticity.js
@@ -1,7 +1,22 @@
 const { compare } = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { securePassword } = require('../util/security');
-const keys = require('../keys/keys.js');
+const { jwtKey } = require('../keys/keys.js');
 const User = require('../models/User');
+
+// Set the expiration time for the web token
+const EXPIRE = { expiresIn: 3600 };
+const AUTH_ERROR = { auth: 'Invalid email or password' };
+
+const assignToken = function(payload, res) {
+  jwt.sign(payload, jwtKey, EXPIRE, (err, token) => {
+    if (err) throw err;
+    res.json({
+      success: true,
+      token: `Bearer ${token}`,
+    });
+  });
+}
 
 module.exports = {
   registerUser: function (user, req, res) {
@@ -31,18 +46,18 @@ module.exports = {
   },
 
   loginUser: function (user, password, res) {
-    const authError = { auth: 'Invalid email or password' };
-
     if (!user) {
-      return res.status(401).json(authError);
+      return res.status(401).json(AUTH_ERROR);
     }
 
     compare(password, user.password)
       .then(isMatch => {
         if (isMatch) {
-          res.json({ msg: 'Success' });
+          const { id, email } = user;
+          const payload = { id, email };
+          assignToken(payload, res);
         } else {
-          return res.status(401).json(authError);
+          return res.status(401).json(AUTH_ERROR);
         }
       });
   }
