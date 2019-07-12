da41f9b0efd04df1dbb891d2cbff58910ad9f6ab
BCrawfordScott <bscott@appacademy.io>
2019-07-12 16:48:27 -0400

    Add validators to users controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,15 +1,32 @@
 const User = require('../models/User');
 const {
   registerUser,
-  loginUser
+  loginUser,
 } = require('../services/authenticity');
+const {
+  validateLogin,
+  validateRegistration,
+} = require('./validations/validations');
 
 module.exports = {
   register: function(req, res) {
-    User.findOne({ email: req.body.email }).then(user => registerUser(user, req, res));
+    const { body } = req;
+    const { errors, isValid } = validateRegistration(body);
+    
+    if (!isValid) return res.status(422).json(errors);
+
+    const { email } = body;
+
+    User.findOne({ email: email }).then(user => registerUser(user, req, res));
   },
   login: function(req, res) {
-    const { email, password } = req.body;
+    const { body } = req;
+    const { errors, isValid } = validateLogin(body);
+
+    if (!isValid) return res.status(422).json(errors);
+
+    const { email, password } = body;
+
     User.findOne({email}).then(user => loginUser(user, password, res));
   },
   current: function (req, res) {
